4dab49cf84e04f9c0dcd1e289e19e2f19b5a9abf
Chris G. Willcocks <christopher.g.willcocks@durham.ac.uk>
2022-04-17 22:51:22 +0100

    removed redundant code

--- .zshrc
+++ .zshrc
@@ -1,7 +1,7 @@
 
 export ZSH="$HOME/.oh-my-zsh"
 
-ZSH_CUSTOM=$HOME/repos/zsh
+ZSH_CUSTOM=$HOME/repos/dotfiles/zsh
 ZSH=$HOME/.oh-my-zsh/
 ZSH_THEME="agnos"
 
--- copy.sh
+++ copy.sh
@@ -6,4 +6,3 @@ cp ~/.dmenurc .
 cp ~/.dir_colors .
 cp ~/.i3/config i3/config
 cp ~/.config/i3status/config i3/i3status
-cp ~/repos/zsh/themes/agnos.zsh-theme zsh/themes/agnos.zsh-theme
